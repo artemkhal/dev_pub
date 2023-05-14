3dfbe89e7296aa7d8ba37eb7526b1e124118981b
artem <artem.khaliullov@gmail.com>
2023-05-14 21:31:49 +0300

    Initial commit

---
+++ src/main/java/com/khaliullov/dev_pub/DevPubApplication.java
@@ -0,0 +1,13 @@
+package com.khaliullov.dev_pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaliullov/dev_pub/DevPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaliullov.dev_pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
